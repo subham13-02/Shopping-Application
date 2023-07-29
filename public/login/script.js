f2a1384e96efb84e3f7d6497a41cf3b502dde5a6
subham13-02 <sahu21019@gmail.com>
2023-07-29 20:39:52 +0530

    updated

---
+++ public/login/script.js
@@ -0,0 +1,11 @@
+// const submit= document.querySelector(".submit-login");
+// const form=document.getElementById("login-form");
+// submit.addEventListener('click',(event)=>{
+//     event.preventDefault();
+//     if(form.elements['email'].value.trim()!=='' && form.elements['password'].value.trim()!==''){
+//         window.location = "../../shop/index.html";
+//     }
+//     else{
+//         alert("All response are Mendatory to fill")
+//     }
+// })
