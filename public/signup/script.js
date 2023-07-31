1edee4b18780bee2952bc8f89029e43e3ba26fd7
subham13-02 <sahu21019@gmail.com>
2023-08-01 02:21:03 +0530

    signup data stored in loacalStorage

---
+++ public/signup/script.js
@@ -0,0 +1,42 @@
+const signup=document.getElementById("signup-form");
+signup.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let fname,lname,mail,password;
+
+    fname=signup.elements['first-name'].value.trim();
+    lname=signup.elements['last-name'].value.trim();
+    email=signup.elements['email'].value.trim();
+    password=signup.elements['password'].value.trim();
+    let msg=document.querySelector(".signup-msg");
+
+    let userData=new Array();
+    if(fname==""||lname==""||password==""){
+        msg.innerHTML=`All the data are compulsory to fill*`;
+        signup.reset();
+        return;
+    }
+
+    if(signup.elements['password'].value.trim()===signup.elements['confirm-password'].value.trim()){
+        userData=JSON.parse(localStorage.getItem("user"))?JSON.parse(localStorage.getItem("user")):[];
+        
+        if(userData.some((e)=>{return e.email==email})){
+            msg.innerHTML=`This Email id Alredy Excist*`;
+        }
+        else{
+            userData.push({
+                "fname":fname,
+                "lname":lname,
+                "email":email,
+                "password":password,
+            });
+            localStorage.setItem("user",JSON.stringify(userData));
+            msg.innerHTML=`<h4>Signup Sucessful !</h2>`;
+            signup.reset();
+        }
+        
+
+    }
+    else{
+       msg.innerHTML=`Passwords Should Be Same*`;
+    }
+});
